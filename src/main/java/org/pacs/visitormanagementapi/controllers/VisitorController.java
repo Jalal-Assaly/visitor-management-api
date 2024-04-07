e438e34cd5f42b0279464dbc09c96c213f55df47
Jalal Assaly <jalal.assaly@gmail.com>
2024-04-07 02:06:09 +0200

    Edit endpoint

--- src/main/java/org/pacs/visitormanagementapi/controllers/VisitorController.java
+++ src/main/java/org/pacs/visitormanagementapi/controllers/VisitorController.java
@@ -43,13 +43,13 @@ public class VisitorController {
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
-    @PutMapping(path = "/update/id/{id}")
+    @PutMapping(path = "/update/{id}")
     public ResponseEntity<Void> updateVisitorAttributes(@RequestBody VisitorModel visitorModel, @PathVariable String id) {
         visitorService.updateVisitor(visitorModel, id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    @DeleteMapping(value = "/delete/id/{id}")
+    @DeleteMapping(value = "/delete/{id}")
     public ResponseEntity<Void> deleteVisitorAttributes(@PathVariable String id) {
         visitorService.deleteVisitorAttributes(id);
         return new ResponseEntity<>(HttpStatus.OK);
