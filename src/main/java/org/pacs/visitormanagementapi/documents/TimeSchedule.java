56c9865733addfd29ac02771cc45f3da22ceae0d
Jalal Assaly <jalal.assaly@gmail.com>
2024-05-13 17:52:55 +0300

    Add missing @JsonProperty

--- src/main/java/org/pacs/visitormanagementapi/documents/TimeSchedule.java
+++ src/main/java/org/pacs/visitormanagementapi/documents/TimeSchedule.java
@@ -1,5 +1,6 @@
 package org.pacs.visitormanagementapi.documents;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -11,10 +12,13 @@ import java.util.Set;
 @Data
 @AllArgsConstructor
 public class TimeSchedule {
+    @JsonProperty("ST")
     @NotNull(message = "Start time can not be null")
     private LocalTime startTime;
+    @JsonProperty("ET")
     @NotNull(message = "The end time can not be null")
     private LocalTime endTime;
+    @JsonProperty("DW")
     @NotEmpty(message = "The week schedule can not be empty")
     private Set<String> daysOfWeek;
 }
