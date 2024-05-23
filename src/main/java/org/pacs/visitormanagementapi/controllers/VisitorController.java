c6318752685dbe88089f663c92b6fa40b7719f28
Jalal Assaly <jalal.assaly@gmail.com>
2024-05-23 17:36:42 +0300

    URL Refactor

--- src/main/java/org/pacs/visitormanagementapi/controllers/VisitorController.java
+++ src/main/java/org/pacs/visitormanagementapi/controllers/VisitorController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/visitor")
+@RequestMapping("/visitor-management")
 public class VisitorController {
 
     private final VisitorService visitorService;
